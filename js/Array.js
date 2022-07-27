7835fa66efb8ab4b4dc12389f4b5f1a4b62df22d
prasanthKminuscule <prasanth@minusculetechnologies.com>
2022-07-27 13:28:57 +0530

    Updated files

--- js/Array.js
+++ js/Array.js
@@ -13,14 +13,18 @@ Employee.Array=(
     a[1]="Siva";
     a[2]=8.2551;
     a[3]=123;
+    a.push(new Date());
     
     var res=[];
     res.push("<ul>")
     
+
+    debugger;
     
     for(i=0;i<a.length;i++)
     {
         res.push("<li>"+a[i]+"</li>");
+        console.log(res.join(" "));
     }
     
     res.push("/<ul>");
@@ -38,9 +42,34 @@ Employee.Array=(
         document.getElementById("conct").innerHTML=z.join(" ");
     }
 
+
+    function TestSlice()
+    {
+        
+        var y=["rpd","mpd","xpd","kp","mp","xp"];
+
+        var z=y.slice(2,6);
+
+        document.getElementById("slice").innerHTML=z.join(" ");
+    }
+    function TestShifts()
+    {
+
+        var n=["prasa","rajesh","Mani"];
+        var s=n.shift();
+        alert(s);
+        document.getElementById("shifts").innerHTML=n.join(" ")
+        n.unshift("PrasanthKP");
+        document.getElementById("unshifts").innerHTML=n.join(" ")
+
+    }
+
     return{
         showList:showList ,
-        TestConcat:TestConcat
+        TestConcat:TestConcat,
+        TestSlice:TestSlice,
+        TestShifts:TestShifts
+
     };
 
 
