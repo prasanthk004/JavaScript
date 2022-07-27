a3d1b3acc8592292d6299f95d83ef5f39a82a8f0
prasanthKminuscule <prasanth@minusculetechnologies.com>
2022-07-27 12:16:20 +0530

    Updated files

--- js/Array.js
+++ js/Array.js
@@ -1,7 +1,53 @@
-var a=[];   
+var Employee=window.Employee||{};
+ 
 
-a[0]="Prasa";
-a[1]="Siva";
-a[3]=123;
+Employee.Array=(
+    
+    function(){
 
-alert(a[0]+" "+a[1]+" "+a[3])
+    function showList()
+    {
+        var a=[];   
+    
+    a[0]="Prasa";
+    a[1]="Siva";
+    a[2]=8.2551;
+    a[3]=123;
+    
+    var res=[];
+    res.push("<ul>")
+    
+    
+    for(i=0;i<a.length;i++)
+    {
+        res.push("<li>"+a[i]+"</li>");
+    }
+    
+    res.push("/<ul>");
+    
+    document.getElementById("list").innerHTML=res.join(" ");
+    }
+    
+    function TestConcat()
+    {
+        var x=["kp","mp","xp"];
+        var y=["rpd","mpd","xpd"];
+
+        var z=x.concat(y);
+
+        document.getElementById("conct").innerHTML=z.join(" ");
+    }
+
+    return{
+        showList:showList ,
+        TestConcat:TestConcat
+    };
+
+
+
+}
+
+)();
+
+
+
