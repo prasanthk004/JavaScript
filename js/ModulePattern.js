9438065c891bb5d2c215a497d555efeb434d0847
prasanthKminuscule <prasanth@minusculetechnologies.com>
2022-07-27 11:19:16 +0530

    Updated files

---
+++ js/ModulePattern.js
@@ -0,0 +1,30 @@
+var Employee=(
+
+
+    function()
+    {
+
+        var name,age,salary;
+        function init(Name,Age)
+        {
+            name=Name;
+            age=Age;
+            salary=age*50;
+
+        }
+        function display()
+        {
+            document.write(name+" |"+age+" |"+salary);
+        }
+        return{
+            Init:init,Display:display
+        };
+    
+
+        
+    }
+)();
+
+     
+    
+    
